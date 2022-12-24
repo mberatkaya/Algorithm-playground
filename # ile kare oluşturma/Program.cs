a6ab990e2003faf2c00bb9e8d71b2ac486eee5b5
mberatkaya <mbkaya879@gmail.com>
2022-12-25 00:50:17 +0300

    i use # to create square

---
+++ # ile kare oluşturma/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xx_Deneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            int a, i;
+
+
+            Console.Write("Kare Uzunluk ve Genişlik Birimi Giriniz = ");
+            sayi = Convert.ToInt16(Console.ReadLine());
+
+            for (a = 1; a <= sayi; a++)
+            {
+                Console.WriteLine("\n");
+
+                for (i = 1; i <= sayi; i++) 
+                {
+                    Console.Write("  # ");                   
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
