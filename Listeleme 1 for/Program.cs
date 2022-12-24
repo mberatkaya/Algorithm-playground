6effd8ab27e13c6c9bec9b9eeaddbabef1db658e
mberatkaya <mbkaya879@gmail.com>
2022-12-25 00:52:41 +0300

    I listed numbers and #s

---
+++ Listeleme 1 for/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V_algoritma_ödevim1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+
+            for (i = 1; i <= 5; i++)
+            {
+                if (i%2 ==1)
+                {
+                    Console.WriteLine("\n");
+                        Console.Write("1  2  3  4  5");
+                }
+                else if (i%2 ==0)
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine("#  #  #  #  # ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
