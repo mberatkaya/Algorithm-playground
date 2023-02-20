e3bcf05e601c09bfaf2cb02caabbca89256a0998
mberatkaya <mbkaya879@gmail.com>
2023-02-20 18:24:12 +0300

    Listeleme 5

---
+++ V listeleme - tag 3v5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V_listeleme___tag_3v5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("-");
+                    }
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
